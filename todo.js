d21bc737de8388a58867405390ad7fbd1e25312c
Zhenkaf <Zhenkaf@mail.ru>
2016-12-10 09:12:49 +0200

    как добраться до конкретного родителя?

--- todo.js
+++ todo.js
@@ -1,26 +1,12 @@
 $(document).ready(function () {
 
-    $("#inputPlus").click(function insertInput (rowTemplate, che) {
-        var rowTemplate = " ";
-        var che = false;
-
-        var input = document.createElement('input');
-        input.id = 'inp';
-        input.type = 'text';
-        input.value = rowTemplate;
-
-        var input2 = document.createElement('input');
-        input2.id = 'ch';
-        input2.type = 'checkbox';
-        input2.value = che;
-
-        allInputs.appendChild(input);
-        allInputs.appendChild(input2);//allInputs - это див который в html
-
+    $("#inputPlus").click(function (rowTemplate, che) {
+     insertInput(" ",false);
     });
 
-    $("#del").click(function () {
-        $(".js-row").last().remove();
+    $("#allInputs").click(function (event) {
+        var t = event.target;
+        t.closest("#wer").remove();
     });
 
     $("#save").click(function () {
@@ -30,11 +16,40 @@ $(document).ready(function () {
 
         localStorage.setItem("key", so);
         console.log(so);
-
     });
     if(localStorage.getItem("key")) {
 
         $("#allInputs").html(JSON.parse(localStorage.getItem("key")));
+    }
+
+    function insertInput (rowTemplate, che) {
+        var div = document.createElement('div');
+            div.id = 'wer';
+
+        var input = document.createElement('input');
+            input.type = 'text';
+            input.value = rowTemplate;
+
+        var input2 = document.createElement('input');
+            input2.type = 'checkbox';
+            input2.value = che;
+
+        var input3 = document.createElement('input');
+            input3.type = 'button';
+            input3.value = 'save';
+
+
+        var input4 = document.createElement('input');
+            input4.type = 'button';
+            input4.value = 'del';
+            input4.id = 'del';
+
+        div.appendChild(input2);
+        div.appendChild(input);
+        div.appendChild(input3);
+        div.appendChild(input4);
+
+        document.getElementById("allInputs").appendChild(div);
     }
 
 });
