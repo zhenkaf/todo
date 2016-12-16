56961fbcf1e7978d6a44eaf2d7e668d99b88c024
Zhenkaf <Zhenkaf@mail.ru>
2016-12-16 23:11:26 +0200

    й

--- todo.js
+++ todo.js
@@ -1,10 +1,14 @@
 $(document).ready(function () {
 
     if(localStorage.getItem("keyMas")) {
-        var n =JSON.parse(localStorage.getItem("keyMas"));
+        var n = JSON.parse(localStorage.getItem("keyMas"));
             for(i = 0; i<n.length; i++){
-                insertInput("",false);
-        }
+              var rowTemplate = n[i].a;
+                var che = n[i].b;
+                alert (rowTemplate);
+                alert (che);
+                insertInput(rowTemplate, che);
+            }
     }
 
     $("#inputPlus").click(function (rowTemplate, che) {
@@ -19,18 +23,13 @@ $(document).ready(function () {
     });
     function insertInput (rowTemplate, che) {
         var div = $("<div class = 'input-s'></div>");
-            $(div).append("<input type = 'checkbox'></input>" +
-                          "<input type = 'text'></input>" +
-                          "<input type = 'button' value = 'save'></input>" +
+            $(div).append("<input type = 'checkbox' class = 'checkbo-x'></input>" +
+                          "<input type = 'text' class = 'tex-t'></input>" +
+                          "<input type = 'button' value = 'save' class = 'sav-e'></input>" +
                           "<input type = 'button' value = 'del' class = 'del-t'></input>"
             );
-
         $('#allInputs').append(div);
 
-
-            //input.value = rowTemplate;
-            //input2.value = che;
-            //input3.id = 'sav';
     }
     function localPlus(str, isDone) {
         if (localStorage.getItem("keyMas") != undefined) {
@@ -54,14 +53,10 @@ $(document).ready(function () {
                 localStorage.setItem("keyMas", serialM);
         }
     }
-
     function delElemInLocStor(event) {
         var t = $(event.target).parents(".input-s");
         var index = $(".input-s").index(t);
-        alert(index);
-
         var serialBack = JSON.parse(localStorage.getItem("keyMas"));
-
         serialBack.splice(index, 1);
         var sB = JSON.stringify(serialBack);
         localStorage.setItem("keyMas", sB);
@@ -69,7 +64,25 @@ $(document).ready(function () {
     function delParagraph(event) {
         var t = $(event.target).parents(".input-s");
         var index = $(".input-s").index(t);
-        alert(index);
         t.remove();
+    }
+    $("#allInputs").on("click", ".sav-e", function (event) {
+        saveInfo(event)
+    });
+    function saveInfo(event) {
+        var ourClick = $(event.target).parents(".input-s");
+        var OurDiv = ourClick.closest(".input-s");
+        var indexSave = $(".input-s").index(ourClick);
+        var st = $(OurDiv).contents(".tex-t").val();
+        var v = $(OurDiv).contents(".checkbo-x");
+        var check =$(v).is(':checked');
+        var obj = {
+            a : st,
+            b : check
+        };
+        var n =JSON.parse(localStorage.getItem("keyMas"));
+         n.splice(indexSave, 1, obj);
+        var newN = JSON.stringify(n);
+        localStorage.setItem("keyMas", newN);
     }
 });
